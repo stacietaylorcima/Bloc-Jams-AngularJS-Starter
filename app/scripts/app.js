aef2037370a1a7565da20414c139317ac76ad529
stacietaylorcima <stacietaylorcima@gmail.com>
2017-11-01 12:09:24 -0700

    fix injected dependancy syntax, add state for collection, refactor href to ui-sref

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,9 +14,13 @@
       .state('album', {
         url: '/album',
         templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url:'/collection',
+        templateUrl: '/templates/collection.html'
       });
   }
     angular
-    .module('blocJams', ['ui-router'])
+    .module('blocJams', ['ui.router'])
     .config(config);
     })();
