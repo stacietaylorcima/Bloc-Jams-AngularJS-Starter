e1d12f592c37e3158f3c7bb98bc46c53351eefa0
stacietaylorcima <stacietaylorcima@gmail.com>
2017-11-06 13:14:39 -0800

    remove uneccessary css files and fix errors on line 13 & 22 of app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
     $stateProvider
       .state('landing', {
         url:'/',
-        controller: 'LandingCtrl as landing'
+        controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
       })
       .state('album', {
@@ -18,7 +18,7 @@
       })
       .state('collection', {
         url:'/collection',
-        collection: "CollectionCtrl as collection"
+        collection: "CollectionCtrl as collection",
         templateUrl: '/templates/collection.html'
       });
   }
