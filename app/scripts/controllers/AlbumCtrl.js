c678fe5c98cf45208bb517addcd0e4223ee6903b
stacietaylorcima <stacietaylorcima@gmail.com>
2017-11-07 13:56:22 -0800

    use ng-repeat to add a song row for each song on the album, replace static html with angular expression markup

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,11 @@
 (function() {
+
   function AlbumCtrl() {
-    this.albumData = [];
-    for (var i=0; i < albumPicasso.songs.length; i++) {
-      this.songs.push(angular.copy(albumPicasso));
-    }
-  }
+    this.albumData = angular.copy(albumPicasso);
+  };
 
   angular
-    .module("blocJams")
-    .controller("AlbumCtrl", AlbumCtrl)
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
+
 })();
