a4d7ea311432aa1745fa3cf39626a2d27bea8eb1
stacietaylorcima <stacietaylorcima@gmail.com>
2017-11-06 18:52:51 -0800

    create controller for album view, ngRepeat on the album-view-song-item table row, replace static html using {{}} markup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,11 +14,12 @@
       })
       .state('album', {
         url: '/album',
+        controller: "AlbumCtrl as album",
         templateUrl: '/templates/album.html'
       })
       .state('collection', {
         url:'/collection',
-        collection: "CollectionCtrl as collection",
+        controller: "CollectionCtrl as collection",
         templateUrl: '/templates/collection.html'
       });
   }
