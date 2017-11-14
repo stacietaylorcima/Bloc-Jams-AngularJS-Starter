087ecb2fa057dffbec5d4ecc98d93c9bb89c305a
stacietaylorcima <stacietaylorcima@gmail.com>
2017-11-13 21:05:07 -0800

    refactor play method to make parts of the code reusable and create a setSong fucntion using that code

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,43 +1,43 @@
 (function() {
-  function SongPlayer() {
-    var SongPlayer = {};
+	function SongPlayer() {
+		var SongPlayer = {};
+		var currentSong = null;
 
-    var currentSong = null;
     var currentBuzzObject = null;
 
-    SongPlayer.play = function(song) {
+    var setSong = function(song) {
+			if (currentBuzzObject) {
+				currentBuzzObject.stop();
+				currentSong.playing = null;
+			}
+			currentBuzzObject = new buzz.sound(song.audioUrl, {
+				formats: ['mp3'],
+				preload: true
+			});
+			currentSong = song;
+		};
 
-      if (currentSong !== song) {
-           if (currentBuzzObject) {
-               currentBuzzObject.stop();
-               currentSong.playing = null;
-           } else if (currentSong === song) {
-         if (currentBuzzObject.isPaused()) {
-             currentBuzzObject.play();
-         }
-     }
+  SongPlayer.play = function(song) {
+		if (SongPlayer.currentSong !== song) {
+			setSong(song);
+			currentBuzzObject.play();
+			song.playing = true;
+  		} else if (SongPlayer.currentSong === song) {
+  			if (currentBuzzObject.isPaused()) {
+  				currentBuzzObject.play();
+  			}
+  		}
+  	};
 
-
-     currentBuzzObject = new buzz.sound(song.audioUrl, {
-       formats: ['mp3'],
-       preload: true
-     });
-
-      currentSong = song;
-
-      currentBuzzObject.play();
-      song.playing = true;
-    }
-  }
-    return SongPlayer;
-  };
-
-  SongPlayer.pause = function(song) {
+    SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
-  };
+    };
 
-  angular
-  .module('blocJams')
-  .factory('SongPlayer', SongPlayer);
+  	return SongPlayer;
+	}
+
+
+
+  angular.module('blocJams').factory('SongPlayer', SongPlayer);
 })();
