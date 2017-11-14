dbfdcf754515011732d1078d992a0c1efee67490
stacietaylorcima <stacietaylorcima@gmail.com>
2017-11-13 17:19:22 -0800

    add a pause method to SongPlayer.js and reflect that method on album.html

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -10,40 +10,34 @@
       if (currentSong !== song) {
            if (currentBuzzObject) {
                currentBuzzObject.stop();
+               currentSong.playing = null;
            } else if (currentSong === song) {
          if (currentBuzzObject.isPaused()) {
              currentBuzzObject.play();
          }
      }
 
+
      currentBuzzObject = new buzz.sound(song.audioUrl, {
-<<<<<<< HEAD
        formats: ['mp3'],
        preload: true
-   });
-=======
-        formats: ['mp3'],
-        preload: true
-      });
->>>>>>> 82514a40cff586867a241e34f32fa88de68c71d2
+     });
 
       currentSong = song;
 
       currentBuzzObject.play();
-    };
-<<<<<<< HEAD
-}
-=======
-
->>>>>>> 82514a40cff586867a241e34f32fa88de68c71d2
-    return SongPlayer;
+      song.playing = true;
+    }
   }
+    return SongPlayer;
+  };
+
+  SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+  };
 
   angular
   .module('blocJams')
   .factory('SongPlayer', SongPlayer);
-<<<<<<< HEAD
 })();
-=======
-}();
->>>>>>> 82514a40cff586867a241e34f32fa88de68c71d2
